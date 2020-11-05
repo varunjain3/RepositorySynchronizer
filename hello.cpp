5073b97f65e0712510eb2fef7c6aee03989fef2f
varunjain3 <varun.jain@iitgn.ac.in>
2020-11-05 10:48:29 +0530

    add and remove files added

--- hello.cpp
+++ hello.cpp
@@ -15,21 +15,26 @@ struct filestat
 typedef map<string, filestat> filemap;
 typedef pair<string, filestat> fileobject;
 // Watchdog file watcher
+
 class WatchDog
 {
-private:
+
 public:
     filemap Log;
     // Constructor, will initialize the Watchdog
-    WatchDog();
-    WatchDog(string path)
-    {
-        this->Log = getLog(path);
-    }
+    // WatchDog();
+    // WatchDog(string path)
+    // {
+    //     this->Log = getLog(path);
+    // }
 
-    void addkeyvalue(filemap newlog, string key, filestat value)
+    void addkeyvalue(filemap &newlog, string key, filestat value)
     {
         newlog.insert(fileobject(key, value));
+    }
+    void addkey2self(string key, filestat value)
+    {
+        this->Log.insert(fileobject(key, value));
     }
 
     string gethash(string key)
@@ -58,19 +63,52 @@ public:
         this->addkeyvalue(newlog, "path", temp);
         return newlog;
     }
+
+    // Add Files as per given src and destination
+    // bool copyFile(const char *SRC, const char *DEST)
+    // {
+    //     ifstream src(SRC, ios::binary);
+    //     ofstream dest(DEST, ios::binary);
+    //     dest << src.rdbuf();
+    //     return src && dest;
+    // }
+
+    bool copyFile(string srcfolder, string destfolder, string filename)
+    {
+        string SRC = srcfolder + "/" + filename;
+        string DEST = destfolder + "/" + filename;
+
+        ifstream src(SRC, ios::binary);
+        ofstream dest(DEST, ios::binary);
+        dest << src.rdbuf();
+        return src && dest;
+    }
+
+    // removes files as per given path
+    bool delFile(string path)
+    {
+        return (remove(path.c_str()) != 0) ? 0 : 1;
+    }
 };
 
 int main(int argc, char *argv[])
 {
 
-    WatchDog g();
+    WatchDog g;
+    filemap a;
     filestat temp;
+    string folder1 = "test1";
+    string folder2 = "test2";
+
     temp.folder = true;
     temp.hash = "path";
-    g.addkeyvalue(g.Log, "myworld", temp);
+    g.addkeyvalue(g.Log, "Hello", temp);
+    g.addkey2self("myworld", temp);
     temp.folder = true;
     temp.hash = "path2";
-    g.addkeyvalue(g.Log, "helloworld", temp);
-    g.gethash("myworld");
-    g.gethash("helloworld");
+    g.addkey2self("helloworld", temp);
+    cout << g.gethash("myworld") << endl;
+    cout << g.gethash("helloworld") << endl;
+    g.copyFile(folder1, folder2, "file1.txt");
+    g.delFile("test2/file1.txt");
 }
